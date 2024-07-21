aa29a3c78873650d8225322e97a9650da8ed9023
janmajay6 <89271993+janmajay6@users.noreply.github.com>
2024-07-21 16:34:41 +0530

    Update Hero.tsx

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Hero() {
   return (
     <section className="bg-gray-900 text-white">
-  <div className="mx-auto max-w-screen-xl px-4 py-20 lg:flex  ">
+  <div className="mx-auto max-w-screen-xl px-4 py-96 lg:flex  ">
     <div className="mx-auto max-w-3xl text-center">
       <h1
         className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl "
